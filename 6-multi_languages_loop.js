e49c3a0c831de36882ab0dbae72e8e1b587bd97d
Kalu Jonah Chinonso <kaluchinonso18@gmail.com>
2025-07-13 18:58:46 +0100

    A script that prints 3 Lines of codes by using an array of string and a for loop.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,15 @@
-const statements = ["c is fun", "python is cool", "JavaScript is amazing"];
+// const statements = ["c is fun", "python is cool", "JavaScript is amazing"];
 
-for (let i = 0; i < 3; i++){
-console.log(statements[i]);
-} 
+// for (let i = 0; i < 3; i++){
+// console.log(statements[i]);
+// } 
+
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (const index in messages) {
+  console.log(messages[index]);
+}
